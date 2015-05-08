663242a78d00db433342bfc344122936f600a32c
Ken Wilcox <wilcoxk@gmail.com>
2015-05-08 00:35:08 -0600

    using the id

---
+++ ProductsModule.cs
@@ -0,0 +1,15 @@
+namespace NancyApplication
+{
+    using Nancy;
+	
+	public class ProductsModule : NancyModule
+{
+    public ProductsModule()
+    {
+        Get["/products/{id}"] = _ =>
+        {
+            return "The product is " + _.id + "<br>" + _.id.Value.GetType();
+        };
+    }
+}
+}
