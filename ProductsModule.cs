6aee820c477b1343e8a7454c1c2f5f782af98378
Ken Wilcox <wilcoxk@gmail.com>
2015-05-10 23:16:09 -0600

    Formatting

--- ProductsModule.cs
+++ ProductsModule.cs
@@ -3,13 +3,13 @@ namespace NancyApplication
     using Nancy;
 	
 	public class ProductsModule : NancyModule
-{
-    public ProductsModule()
     {
-        Get["/products/{id}"] = _ =>
+        public ProductsModule()
         {
-            return "The product is " + _.id + "<br>" + _.id.Value.GetType();
-        };
+            Get["/products/{id}"] = _ =>
+            {
+                return "The product is " + _.id + "<br>" + _.id.Value.GetType();
+            };
+        }
     }
-}
 }
